8cb8d75cf4a5bab8af4ebe625c83a9e952f3ca04
dumerrill <dumerrill@nvidia.com>
2013-03-07 10:46:27 -0500

    Update main web page and grouping of thread load/store operations.
    Former-commit-id: a59e4f19e52d4e918e091f949ed4f5622d2239f1

--- search/enums_70.js
+++ search/enums_70.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ptxloadmodifier',['PtxLoadModifier',['../namespacecub.html#a023420f30fec7d4b187fc98f4fd2a55d',1,'cub']]],
-  ['ptxstoremodifier',['PtxStoreModifier',['../namespacecub.html#ae9c7d6a6af7104f528509182ac9c9da2',1,'cub']]]
+  ['ptxloadmodifier',['PtxLoadModifier',['../group___simt_utils.html#ga023420f30fec7d4b187fc98f4fd2a55d',1,'cub']]],
+  ['ptxstoremodifier',['PtxStoreModifier',['../group___simt_utils.html#gae9c7d6a6af7104f528509182ac9c9da2',1,'cub']]]
 ];
