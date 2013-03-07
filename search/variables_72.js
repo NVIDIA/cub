12efa912fae18520ff79c1a5d1eaf2cc1b00a697
dumerrill <dumerrill@nvidia.com>
2013-03-07 12:43:57 -0500

    Update front page
    Former-commit-id: 20d2937b998721224ef13cb7c0be527d98aa4b08

--- search/variables_72.js
+++ search/variables_72.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['raking_5fgrid',['raking_grid',['../structcub_1_1_block_scan_1_1_block_scan_internal_3_01_b_l_o_c_k___s_c_a_n___r_a_k_i_n_g_00_01_d_u_m_m_y_01_4_1_1_smem_storage.html#a5dd810ac5af7a5b4d33995d18f73ddd0',1,'cub::BlockScan::BlockScanInternal&lt; BLOCK_SCAN_RAKING, DUMMY &gt;::SmemStorage']]]
+  ['raking_5fgrid',['raking_grid',['../structcub_1_1_block_scan_1_1_block_scan_internal_1_1_smem_storage.html#ab41dbf077ce7b3544f79af41a3cffbf4',1,'cub::BlockScan::BlockScanInternal::SmemStorage']]]
 ];
