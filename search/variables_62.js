6b1ec2cc0b5b5894349ec0a35c1169f0072f3168
dumerrill <dumerrill@nvidia.com>
2013-03-07 13:34:30 -0500

    Project name was renamed to lower case
    Former-commit-id: 63c91d18206c9b8579c2bbf4790dca6fd43dfb71

--- search/variables_62.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['block_5fprefix',['block_prefix',['../structcub_1_1_block_scan_1_1_block_scan_internal_3_01_b_l_o_c_k___s_c_a_n___w_a_r_p_s_c_a_n_s_001c3295e9951a81a5136fa5f4f99e4f46.html#a3b3f81a0323dc59adf0bf897bbdb5281',1,'cub::BlockScan::BlockScanInternal&lt; BLOCK_SCAN_WARPSCANS, DUMMY &gt;::SmemStorage']]],
-  ['broadcast',['broadcast',['../structcub_1_1_warp_scan_1_1_warp_scan_internal_3_01_s_m_e_m___s_c_a_n_00_01_d_u_m_m_y_01_4_1_1_smem_storage.html#a407365ccd9b19aca6d39d5fba25258c0',1,'cub::WarpScan::WarpScanInternal&lt; SMEM_SCAN, DUMMY &gt;::SmemStorage']]]
-];
--- search/variables_72.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['raking_5fgrid',['raking_grid',['../structcub_1_1_block_scan_1_1_block_scan_internal_1_1_smem_storage.html#ab41dbf077ce7b3544f79af41a3cffbf4',1,'cub::BlockScan::BlockScanInternal::SmemStorage']]]
-];
