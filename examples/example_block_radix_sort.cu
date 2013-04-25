5fa6491de379633efe92995eff3925f62db2361b
dumerrill <dumerrill@nvidia.com>
2013-04-25 13:18:02 -0400

    Added new scan algorithm variant
    Former-commit-id: 1e515f6839467a83f41e4908d17675436fc511e4

--- examples/example_block_radix_sort.cu
+++ examples/example_block_radix_sort.cu
@@ -77,34 +77,133 @@ int g_grid_size = 1;
  * Simple kernel for performing a block-wide sorting over integers
  */
 template <
+    typename    KeyType,
     int         BLOCK_THREADS,
     int         ITEMS_PER_THREAD>
 __global__ void BlockSortKernel(
-    int         *d_in,          // Tile of input
-    int         *d_out,         // Tile of output
+    KeyType     *d_in,          // Tile of input
+    KeyType     *d_out,         // Tile of output
     clock_t     *d_elapsed)     // Elapsed cycle count of block scan
 {
+    enum { TILE_SIZE = BLOCK_THREADS * ITEMS_PER_THREAD };
+
     // Parameterize BlockRadixSort type for our thread block
-    typedef BlockRadixSort<int, BLOCK_THREADS, ITEMS_PER_THREAD> BlockRadixSortT;
+    typedef BlockRadixSort<KeyType, BLOCK_THREADS, ITEMS_PER_THREAD> BlockRadixSortT;
 
     // Shared memory
     __shared__ typename BlockRadixSortT::SmemStorage smem_storage;
 
-    // Per-thread tile data
-    int data[ITEMS_PER_THREAD];
-    BlockLoadVectorized(d_in, data);
+    // Per-thread tile items
+    KeyType items[ITEMS_PER_THREAD];
+
+    // Load items
+    int block_offset = blockIdx.x * TILE_SIZE;
+    if (ITEMS_PER_THREAD % 4 == 0)
+    {
+        // Vectorize
+        typedef VectorHelper<KeyType, 4> VecHelper;
+        typedef typename VecHelper::Type VectorT;
+
+        // Alias items as an array of VectorT and load it in striped fashion
+        BlockLoadDirectStriped(
+            reinterpret_cast<VectorT*>(d_in + block_offset),
+            reinterpret_cast<VectorT (&)[ITEMS_PER_THREAD / 4]>(items));
+    }
+    else
+    {
+        BlockLoadDirectStriped(d_in + block_offset, items);
+    }
 
     // Start cycle timer
     clock_t start = clock();
 
     // Sort keys
-    BlockRadixSortT::SortBlocked(smem_storage, data);
+    BlockRadixSortT::SortBlockedToStriped(smem_storage, items);
 
     // Stop cycle timer
     clock_t stop = clock();
 
     // Store output
-    BlockStoreVectorized(d_out, data);
+    BlockStoreDirectStriped(d_out + block_offset, items);
+
+    // Store elapsed clocks
+    if (threadIdx.x == 0)
+    {
+        *d_elapsed = (start > stop) ? start - stop : stop - start;
+    }
+
+}
+
+/**
+ * Simple kernel for performing a block-wide sorting over integers
+ */
+template <
+    typename    KeyType,
+    int         BLOCK_THREADS,
+    int         ITEMS_PER_THREAD>
+__global__ void BlockSortKernel2(
+    KeyType     *d_in,          // Tile of input
+    KeyType     *d_out,         // Tile of output
+    clock_t     *d_elapsed)     // Elapsed cycle count of block scan
+{
+    enum { TILE_SIZE = BLOCK_THREADS * ITEMS_PER_THREAD };
+
+    __shared__ unsigned int histogram[256];
+    __shared__ volatile unsigned int name[256];
+
+    // Initialize histo
+    if (threadIdx.x < 32)
+    {
+        for (int base = 0; base < 256; base += 32)
+        {
+            histogram[base + threadIdx.x] = 0;
+        }
+    }
+
+    __syncthreads();
+
+    // Per-thread tile items
+    KeyType items[ITEMS_PER_THREAD];
+
+    // Load items
+    int block_offset = blockIdx.x * TILE_SIZE;
+    if (ITEMS_PER_THREAD % 4 == 0)
+    {
+        // Vectorize
+        typedef VectorHelper<KeyType, 4> VecHelper;
+        typedef typename VecHelper::Type VectorT;
+
+        // Alias items as an array of VectorT and load it in striped fashion
+        BlockLoadDirectStriped(
+            reinterpret_cast<VectorT*>(d_in + block_offset),
+            reinterpret_cast<VectorT (&)[ITEMS_PER_THREAD / 4]>(items));
+    }
+    else
+    {
+        BlockLoadDirectStriped(d_in + block_offset, items);
+    }
+
+    // Start cycle timer
+    clock_t start = clock();
+
+    // Update histogram
+    #pragma unroll
+    for (int i = 0; i < ITEMS_PER_THREAD; ++i)
+    {
+          atomicAdd(histogram + items[i], 1);
+    }
+
+    // Stop cycle timer
+    clock_t stop = clock();
+
+    // Store output
+    if (threadIdx.x < 32)
+    {
+        for (int base = 0; base < 256; base += 32)
+        {
+            d_out[block_offset + base + threadIdx.x] = histogram[base + threadIdx.x];
+        }
+    }
 
     // Store elapsed clocks
     if (threadIdx.x == 0)
@@ -123,14 +222,16 @@ __global__ void BlockSortKernel(
 /**
  * Initialize sorting problem (and solution).
  */
+template <typename KeyType>
 void Initialize(
-    int *h_in,
-    int *h_reference,
+    KeyType *h_in,
+    KeyType *h_reference,
     int num_items)
 {
     for (int i = 0; i < num_items; ++i)
     {
-        h_in[i] = (int) rand();
+//        RandomBits(h_in[i]);
+        h_in[i] = 0;
         h_reference[i] = h_in[i];
     }
 
@@ -142,26 +243,27 @@ void Initialize(
  * Test BlockScan
  */
 template <
-    int BLOCK_THREADS,
-    int ITEMS_PER_THREAD>
+    typename    KeyType,
+    int         BLOCK_THREADS,
+    int         ITEMS_PER_THREAD>
 void Test()
 {
     const int TILE_SIZE = BLOCK_THREADS * ITEMS_PER_THREAD;
 
     // Allocate host arrays
-    int *h_in           = new int[TILE_SIZE];
-    int *h_reference    = new int[TILE_SIZE];
-    int *h_gpu          = new int[TILE_SIZE];
+    KeyType *h_in           = new KeyType[TILE_SIZE * g_grid_size];
+    KeyType *h_reference    = new KeyType[TILE_SIZE * g_grid_size];
+    KeyType *h_gpu          = new KeyType[TILE_SIZE];
 
     // Initialize problem and reference output on host
-    Initialize(h_in, h_reference, TILE_SIZE);
+    Initialize(h_in, h_reference, TILE_SIZE * g_grid_size);
 
     // Initialize device arrays
-    int *d_in           = NULL;
-    int *d_out          = NULL;
+    KeyType *d_in       = NULL;
+    KeyType *d_out      = NULL;
     clock_t *d_elapsed  = NULL;
-    CubDebugExit(cudaMalloc((void**)&d_in,          sizeof(int) * TILE_SIZE));
-    CubDebugExit(cudaMalloc((void**)&d_out,         sizeof(int) * (TILE_SIZE)));
+    CubDebugExit(cudaMalloc((void**)&d_in,          sizeof(KeyType) * TILE_SIZE * g_grid_size));
+    CubDebugExit(cudaMalloc((void**)&d_out,         sizeof(KeyType) * TILE_SIZE * g_grid_size));
     CubDebugExit(cudaMalloc((void**)&d_elapsed,     sizeof(clock_t)));
 
     // Display input problem data
@@ -169,31 +271,39 @@ void Test()
     {
         printf("Input data: ");
         for (int i = 0; i < TILE_SIZE; i++)
-            printf("%d, ", h_in[i]);
+            std::cout << h_in[i] << ", ";
         printf("\n\n");
     }
 
-    // Copy problem to device
-    CubDebugExit(cudaMemcpy(d_in, h_in, sizeof(int) * TILE_SIZE, cudaMemcpyHostToDevice));
+    // CUDA device props
+    Device device;
+    int max_sm_occupancy;
+    CubDebugExit(device.Init());
+    CubDebugExit(device.MaxSmOccupancy(max_sm_occupancy, BlockSortKernel<KeyType, BLOCK_THREADS, ITEMS_PER_THREAD>, BLOCK_THREADS));
 
-    printf("BlockRadixSort %d items (%d timing iterations, %d blocks, %d threads, %d items per thread): ",
-        TILE_SIZE * g_grid_size, g_iterations, g_grid_size, BLOCK_THREADS, ITEMS_PER_THREAD);
+    // Copy problem to device
+    CubDebugExit(cudaMemcpy(d_in, h_in, sizeof(KeyType) * TILE_SIZE * g_grid_size, cudaMemcpyHostToDevice));
+
+    printf("BlockRadixSort %d items (%d timing iterations, %d blocks, %d threads, %d items per thread, %d SM occupancy):\n",
+        TILE_SIZE * g_grid_size, g_iterations, g_grid_size, BLOCK_THREADS, ITEMS_PER_THREAD, max_sm_occupancy);
+    fflush(stdout);
 
     // Run kernel once to prime caches and check result
-    BlockSortKernel<BLOCK_THREADS, ITEMS_PER_THREAD><<<g_grid_size, BLOCK_THREADS>>>(
+    BlockSortKernel2<KeyType, BLOCK_THREADS, ITEMS_PER_THREAD><<<g_grid_size, BLOCK_THREADS>>>(
         d_in,
         d_out,
         d_elapsed);
 
     // Check for kernel errors and STDIO from the kernel, if any
     CubDebugExit(cudaDeviceSynchronize());
-
+/*
     // Check results
     printf("\tOutput items: ");
     int compare = CompareDeviceResults(h_reference, d_out, TILE_SIZE, g_verbose, g_verbose);
     printf("%s\n", compare ? "FAIL" : "PASS");
     AssertEquals(0, compare);
-
+    fflush(stdout);
+*/
     // Run this several times and average the performance results
     GpuTimer    timer;
     float       elapsed_millis          = 0.0;
@@ -201,13 +311,10 @@ void Test()
 
     for (int i = 0; i < g_iterations; ++i)
     {
-        // Copy problem to device
-        CubDebugExit(cudaMemcpy(d_in, h_in, sizeof(int) * TILE_SIZE, cudaMemcpyHostToDevice));
-
         timer.Start();
 
         // Run kernel
-        BlockSortKernel<BLOCK_THREADS, ITEMS_PER_THREAD><<<g_grid_size, BLOCK_THREADS>>>(
+        BlockSortKernel2<KeyType, BLOCK_THREADS, ITEMS_PER_THREAD><<<g_grid_size, BLOCK_THREADS>>>(
             d_in,
             d_out,
             d_elapsed);
@@ -227,13 +334,16 @@ void Test()
     // Display timing results
     float avg_millis            = elapsed_millis / g_iterations;
     float avg_items_per_sec     = float(TILE_SIZE * g_grid_size) / avg_millis / 1000.0;
+/*
     float avg_clocks            = float(elapsed_clocks) / g_iterations;
     float avg_clocks_per_item   = avg_clocks / TILE_SIZE;
 
-    printf("\tAverage kernel millis: %.4f\n", avg_millis);
-    printf("\tAverage million items / sec: %.4f\n", avg_items_per_sec);
     printf("\tAverage BlockRadixSort::SortBlocked clocks: %.3f\n", avg_clocks);
     printf("\tAverage BlockRadixSort::SortBlocked clocks per item: %.3f\n", avg_clocks_per_item);
+*/
+    printf("\tAverage kernel millis: %.4f\n", avg_millis);
+    printf("\tAverage million items / sec: %.4f\n", avg_items_per_sec);
+    fflush(stdout);
 
     // Cleanup
     if (h_in) delete[] h_in;
@@ -270,9 +380,65 @@ int main(int argc, char** argv)
 
     // Initialize device
     CubDebugExit(args.DeviceInit());
+    fflush(stdout);
 
     // Run tests
-    Test<128, 16>();
+/*
+    printf("\nuint32:\n"); fflush(stdout);
+    Test<unsigned int, 128, 16>();
+    printf("\n"); fflush(stdout);
+
+    printf("\nuint8:\n"); fflush(stdout);
+    Test<unsigned char, 128, 16>();
+    printf("\n"); fflush(stdout);
+
+    printf("\nfp32:\n"); fflush(stdout);
+    Test<float, 128, 16>();
+    printf("\n"); fflush(stdout);
+*/
+
+    Test<unsigned char, 256, 20>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 256, 17>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 256, 16>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 256, 8>();
+    printf("\n"); fflush(stdout);
+
+    printf("\n"); fflush(stdout);
+
+    Test<unsigned char, 128, 20>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 128, 17>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 128, 16>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 128, 8>();
+    printf("\n"); fflush(stdout);
+
+    printf("\n"); fflush(stdout);
+
+    Test<unsigned char, 96, 20>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 96, 17>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 96, 16>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 96, 8>();
+    printf("\n"); fflush(stdout);
+
+    printf("\n"); fflush(stdout);
+
+    Test<unsigned char, 64, 20>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 64, 17>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 64, 16>();
+    printf("\n"); fflush(stdout);
+    Test<unsigned char, 64, 8>();
+    printf("\n"); fflush(stdout);
+
 
     return 0;
 }
