98fc2d8980143f2d21073249333cb908d51c6bfe
dumerrill <dumerrill@nvidia.com>
2013-05-01 05:32:50 -0400

    Dox

--- docs/html/search/functions_68.js
+++ docs/html/search/functions_68.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['histogram',['Histogram',['../classcub_1_1_block_histo256.html#a8ed73296dcac29fd0993b638904c52ea',1,'cub::BlockHisto256']]]
+  ['histogram',['Histogram',['../classcub_1_1_block_histo256.html#a6496bd253fd4611a5722d6c6e7af4b4d',1,'cub::BlockHisto256']]]
 ];
