3850d97696678eb36f915592ad3415309c00b2ed
dumerrill <dumerrill@nvidia.com>
2013-05-01 05:32:50 -0400

    Dox
    Former-commit-id: 98fc2d8980143f2d21073249333cb908d51c6bfe

--- docs/html/search/all_63.js
+++ docs/html/search/all_63.js
@@ -1,11 +1,11 @@
 var searchData=
 [
   ['cachingdeviceallocator',['CachingDeviceAllocator',['../structcub_1_1_caching_device_allocator.html',1,'cub']]],
-  ['cachingdeviceallocator',['CachingDeviceAllocator',['../structcub_1_1_caching_device_allocator.html#a603ab42b997facc30d9337fe6f56144b',1,'cub::CachingDeviceAllocator::CachingDeviceAllocator(unsigned int bin_growth, unsigned int min_bin, unsigned int max_bin, size_t max_cached_bytes)'],['../structcub_1_1_caching_device_allocator.html#a46b26e44e196746698ddd6c2cef2509c',1,'cub::CachingDeviceAllocator::CachingDeviceAllocator()']]],
+  ['cachingdeviceallocator',['CachingDeviceAllocator',['../structcub_1_1_caching_device_allocator.html#a603ab42b997facc30d9337fe6f56144b',1,'cub::CachingDeviceAllocator::CachingDeviceAllocator(unsigned int bin_growth, unsigned int min_bin, unsigned int max_bin, size_t max_cached_bytes)'],['../structcub_1_1_caching_device_allocator.html#a12f2ea29bf0a27eedf85d43604469780',1,'cub::CachingDeviceAllocator::CachingDeviceAllocator(bool silent_cleanup=false)']]],
   ['callback',['Callback',['../classcub_1_1_device.html#a3c986a6aa2d3b69fe8a7ab14c85a6477',1,'cub::Device']]],
   ['cast',['Cast',['../structcub_1_1_cast.html',1,'cub']]],
   ['category',['CATEGORY',['../structcub_1_1_base_traits.html#a25ff6477c84dc3bd5f4b5e70cd600f09',1,'cub::BaseTraits::CATEGORY()'],['../group___util_module.html#ga4733b6d40e923244502e6f5b200766ef',1,'cub::Category()']]],
-  ['composite',['Composite',['../classcub_1_1_block_histo256.html#a82eb5ed83d7428e24c818154bb692fb9',1,'cub::BlockHisto256']]],
+  ['composite',['Composite',['../classcub_1_1_block_histo256.html#a73ead0815070a889b36b13171abcca2e',1,'cub::BlockHisto256']]],
   ['cub',['cub',['../namespacecub.html',1,'']]],
   ['cub_2ecuh',['cub.cuh',['../cub_8cuh.html',1,'']]],
   ['cub_5fcnp_5fenabled',['CUB_CNP_ENABLED',['../group___util_module.html#ga9b4969946073d1c94ec927991f8b3a6b',1,'util_arch.cuh']]],
@@ -13,6 +13,7 @@ var searchData=
   ['cub_5fdestructor',['CUB_DESTRUCTOR',['../group___util_module.html#ga756ebe2b0566abfdfcd3c13fe7d1e9c6',1,'util_arch.cuh']]],
   ['cub_5fptx_5farch',['CUB_PTX_ARCH',['../group___util_module.html#ga76769ce380739b38e0bd00f57e0dbe4f',1,'util_arch.cuh']]],
   ['cubdebug',['CubDebug',['../group___util_module.html#ga84c3a4c178bf6593e0fad2b763606236',1,'util_debug.cuh']]],
+  ['cubdebug2',['CubDebug2',['../group___util_module.html#gaf5994e3dce3f85e8fb00fc175303e545',1,'util_debug.cuh']]],
   ['cubdebugexit',['CubDebugExit',['../group___util_module.html#ga26211db894893b3cec946e4e537536f8',1,'util_debug.cuh']]],
   ['cublog',['CubLog',['../group___util_module.html#ga6788287a780dc10c443aa1ab5ae9f0db',1,'util_debug.cuh']]]
 ];
