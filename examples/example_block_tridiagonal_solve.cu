50bb966f91921c2c6bd628625a30a923a8548df6
dumerrill <dumerrill@nvidia.com>
2013-08-16 10:43:29 -0400

    Revert "debug instability"
    This reverts commit 8366abbe9fd59caa40bf4e5783250b421afdc3f6 [formerly c77b7e04cf927a0bec78b4091f08ae9983cdf9c7].
    
    
    Former-commit-id: f9339292a4e013150b896eace805cb8d71773ab6

--- examples/example_block_tridiagonal_solve.cu
+++ examples/example_block_tridiagonal_solve.cu
@@ -81,12 +81,10 @@ __global__ void BlockSolveKernel(
     typedef BlockTridiagonalSolve<T, BLOCK_THREADS, SCAN_ALGORITHM> BlockTridiagonalSolveT;
 
     // Specialize BlockLoad type for our thread block
-//    typedef BlockLoad<T*, BLOCK_THREADS, ITEMS_PER_THREAD, BLOCK_LOAD_WARP_TRANSPOSE> BlockLoadT;
-    typedef BlockLoad<T*, BLOCK_THREADS, ITEMS_PER_THREAD, BLOCK_LOAD_DIRECT> BlockLoadT;
+    typedef BlockLoad<T*, BLOCK_THREADS, ITEMS_PER_THREAD, BLOCK_LOAD_WARP_TRANSPOSE> BlockLoadT;
 
     // Specialize BlockStore type for our thread block
-//    typedef BlockStore<T*, BLOCK_THREADS, ITEMS_PER_THREAD, BLOCK_STORE_WARP_TRANSPOSE> BlockStoreT;
-    typedef BlockStore<T*, BLOCK_THREADS, ITEMS_PER_THREAD, BLOCK_STORE_DIRECT> BlockStoreT;
+    typedef BlockStore<T*, BLOCK_THREADS, ITEMS_PER_THREAD, BLOCK_STORE_WARP_TRANSPOSE> BlockStoreT;
 
     // Allocate shared memory
     __shared__ union
