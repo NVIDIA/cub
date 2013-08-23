30f90501ff996a0a19b818f8aa2387ec26f15fad
dumerrill <dumerrill@nvidia.com>
2013-08-23 17:29:07 -0400

    v1.0.2 docs
    Former-commit-id: d4dac69fc548e0df0a0e9b78217b5b495cd0a444

--- search/classes_6d.js
+++ search/classes_6d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['max',['Max',['../structcub_1_1_max.html',1,'cub']]]
+  ['max',['Max',['../structcub_1_1_max.html',1,'cub']]],
+  ['min',['Min',['../structcub_1_1_min.html',1,'cub']]]
 ];
