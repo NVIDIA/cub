a4711c1e88d73d77b412e09dc74daaccc7374716
dumerrill <dumerrill@nvidia.com>
2013-12-10 04:40:44 -0500

    Doc update

---
+++ docs/html/search/classes_6b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['keyvaluepair',['KeyValuePair',['../structcub_1_1_key_value_pair.html',1,'cub']]]
+];
---
+++ docs/html/search/variables_76.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value',['value',['../structcub_1_1_item_offset_pair.html#a7faea6eea84a2e5bb4250bd78e765685',1,'cub::ItemOffsetPair::value()'],['../structcub_1_1_key_value_pair.html#a468bef1440a66f45bd9b5193594bf1a4',1,'cub::KeyValuePair::value()']]]
+];
