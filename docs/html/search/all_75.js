72bf5341212cccf9ef70fcf475aeadabac578dbc
dumerrill <dumerrill@nvidia.com>
2014-05-12 17:15:01 -0400

    Updated doc
    Former-commit-id: 21cda361a1bd39c5724673450e3ad76edb94fdde

--- docs/html/search/all_75.js
+++ docs/html/search/all_75.js
@@ -3,7 +3,7 @@ var searchData=
   ['unbindtexture',['UnbindTexture',['../classcub_1_1_tex_obj_input_iterator.html#ad915fda943cade6bb75919bebbb2668b',1,'cub::TexObjInputIterator::UnbindTexture()'],['../classcub_1_1_tex_ref_input_iterator.html#aae8811cd17853d1591fe366e018d7c18',1,'cub::TexRefInputIterator::UnbindTexture()']]],
   ['uninitialized',['Uninitialized',['../structcub_1_1_uninitialized.html',1,'cub']]],
   ['uninitialized_3c_20_5ftempstorage_20_3e',['Uninitialized&lt; _TempStorage &gt;',['../structcub_1_1_uninitialized.html',1,'cub']]],
-  ['unique',['Unique',['../structcub_1_1_device_select.html#ae635fc4f38a07a1c1dd90c177e993312',1,'cub::DeviceSelect']]],
+  ['unique',['Unique',['../structcub_1_1_device_select.html#ae5f491589b45e16197438d884dcda3a1',1,'cub::DeviceSelect']]],
   ['util_5farch_2ecuh',['util_arch.cuh',['../util__arch_8cuh.html',1,'']]],
   ['util_5fdebug_2ecuh',['util_debug.cuh',['../util__debug_8cuh.html',1,'']]],
   ['util_5fdevice_2ecuh',['util_device.cuh',['../util__device_8cuh.html',1,'']]],
