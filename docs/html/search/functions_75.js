21cda361a1bd39c5724673450e3ad76edb94fdde
dumerrill <dumerrill@nvidia.com>
2014-05-12 17:15:01 -0400

    Updated doc

--- docs/html/search/functions_75.js
+++ docs/html/search/functions_75.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['unbindtexture',['UnbindTexture',['../classcub_1_1_tex_obj_input_iterator.html#ad915fda943cade6bb75919bebbb2668b',1,'cub::TexObjInputIterator::UnbindTexture()'],['../classcub_1_1_tex_ref_input_iterator.html#aae8811cd17853d1591fe366e018d7c18',1,'cub::TexRefInputIterator::UnbindTexture()']]],
-  ['unique',['Unique',['../structcub_1_1_device_select.html#ae635fc4f38a07a1c1dd90c177e993312',1,'cub::DeviceSelect']]]
+  ['unique',['Unique',['../structcub_1_1_device_select.html#ae5f491589b45e16197438d884dcda3a1',1,'cub::DeviceSelect']]]
 ];
