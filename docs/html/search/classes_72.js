a51a30f621556c915450e4e88886fb72c0e5c832
dumerrill <dumerrill@nvidia.com>
2014-07-01 10:41:22 -0400

    Adding updated docs
    Former-commit-id: 2891cff8feca7565491afa2b09a66130cc9ae4c3

---
+++ docs/html/search/classes_72.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['reducebysegmentop',['ReduceBySegmentOp',['../classcub_1_1_reduce_by_segment_op.html',1,'cub']]]
+];
---
+++ docs/html/search/functions_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['nontrivialruns',['NonTrivialRuns',['../structcub_1_1_device_run_length_encode.html#a4bd437cbfdd11ceeb893d0f253713ffe',1,'cub::DeviceRunLengthEncode']]]
+];
