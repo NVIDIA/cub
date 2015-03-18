a252c2594e51a312003b314d46edfaa8fedef1dc
dumerrill <dumerrill@moochbot>
2015-03-18 18:52:39 -0400

    doc updates
    Former-commit-id: e8f52e7311f3a9323e87866bdae4cb983a82fe26

--- docs/html/search/enumvalues_62.js
+++ docs/html/search/enumvalues_62.js
@@ -6,6 +6,7 @@ var searchData=
   ['block_5fload_5ftranspose',['BLOCK_LOAD_TRANSPOSE',['../namespacecub.html#a9d7e37497fdd99864c57adecda710401acd94f285472e8f7c883a7407f6f4efc4',1,'cub']]],
   ['block_5fload_5fvectorize',['BLOCK_LOAD_VECTORIZE',['../namespacecub.html#a9d7e37497fdd99864c57adecda710401a826be9d4df1c44c0e5c00a9c9c136965',1,'cub']]],
   ['block_5fload_5fwarp_5ftranspose',['BLOCK_LOAD_WARP_TRANSPOSE',['../namespacecub.html#a9d7e37497fdd99864c57adecda710401aba21066c09e6c6ebcf08c7e86216eb1f',1,'cub']]],
+  ['block_5fload_5fwarp_5ftranspose_5ftimesliced',['BLOCK_LOAD_WARP_TRANSPOSE_TIMESLICED',['../namespacecub.html#a9d7e37497fdd99864c57adecda710401ac054fad7116482974e5cf0ab937bbfa4',1,'cub']]],
   ['block_5freduce_5fraking',['BLOCK_REDUCE_RAKING',['../namespacecub.html#add0251c713859b8974806079e498d10aab32651e17a8a42207e74b7ed8d1aa4d2',1,'cub']]],
   ['block_5freduce_5fraking_5fcommutative_5fonly',['BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY',['../namespacecub.html#add0251c713859b8974806079e498d10aa508e99516231cd77f27295b233ca34b9',1,'cub']]],
   ['block_5freduce_5fwarp_5freductions',['BLOCK_REDUCE_WARP_REDUCTIONS',['../namespacecub.html#add0251c713859b8974806079e498d10aa993903176f938273fa1ff5d4daa808e5',1,'cub']]],
@@ -15,5 +16,6 @@ var searchData=
   ['block_5fstore_5fdirect',['BLOCK_STORE_DIRECT',['../namespacecub.html#a839b145451e9eec3d44172e3c3619700a9b8dcc7b6b06bcfc24af4f499523b880',1,'cub']]],
   ['block_5fstore_5ftranspose',['BLOCK_STORE_TRANSPOSE',['../namespacecub.html#a839b145451e9eec3d44172e3c3619700ab0bbe20613466c3cedfcfea33a97d69c',1,'cub']]],
   ['block_5fstore_5fvectorize',['BLOCK_STORE_VECTORIZE',['../namespacecub.html#a839b145451e9eec3d44172e3c3619700a0ccd625a7f2f3649155cbd5a27adfb41',1,'cub']]],
-  ['block_5fstore_5fwarp_5ftranspose',['BLOCK_STORE_WARP_TRANSPOSE',['../namespacecub.html#a839b145451e9eec3d44172e3c3619700a9178e3be2332f84a66761cee4ee6f3e3',1,'cub']]]
+  ['block_5fstore_5fwarp_5ftranspose',['BLOCK_STORE_WARP_TRANSPOSE',['../namespacecub.html#a839b145451e9eec3d44172e3c3619700a9178e3be2332f84a66761cee4ee6f3e3',1,'cub']]],
+  ['block_5fstore_5fwarp_5ftranspose_5ftimesliced',['BLOCK_STORE_WARP_TRANSPOSE_TIMESLICED',['../namespacecub.html#a839b145451e9eec3d44172e3c3619700ae0f1b9565e96bf7c12d4e02d58ba52d7',1,'cub']]]
 ];
