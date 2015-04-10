8131596c8299753362fa244c01580589a8edfc21
dumerrill <dumerrill@nvidia.com>
2015-04-10 17:09:08 -0400

    update
    Former-commit-id: 5f01380c71edffe9f1c688baad914375c1871316

--- experimental/spmv_compare.cu
+++ experimental/spmv_compare.cu
@@ -81,27 +81,30 @@ void SpmvGold(
     {
         for (OffsetT row = 0; row < a.num_rows; ++row)
         {
+            ValueT partial = 0.0;
             for (
                 OffsetT offset = a.row_offsets[row];
                 offset < a.row_offsets[row + 1];
                 ++offset)
             {
-                vector_y_out[row] += a.values[offset] * vector_x[a.column_indices[offset]];
+                 partial += a.values[offset] * vector_x[a.column_indices[offset]];
             }
+            vector_y_out[row] = partial;
         }
     }
     else
     {
         for (OffsetT row = 0; row < a.num_rows; ++row)
         {
-            vector_y_out[row] = beta * vector_y_in[row];
+            ValueT partial = beta * vector_y_in[row];
             for (
                 OffsetT offset = a.row_offsets[row];
                 offset < a.row_offsets[row + 1];
                 ++offset)
             {
-                vector_y_out[row] += alpha * a.values[offset] * vector_x[a.column_indices[offset]];
+                partial += alpha * a.values[offset] * vector_x[a.column_indices[offset]];
             }
+            vector_y_out[row] = partial;
         }
     }
 
