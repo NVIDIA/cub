f1d2c9ca3677bb611078bae165e9cea6f9fd3454
dumerrill <dumerrill@nvidia.com>
2015-04-15 23:46:56 -0400

    update
    Former-commit-id: badba33074748289958873237eabe331891fe1fd

--- experimental/spmv_script.sh
+++ experimental/spmv_script.sh
@@ -4,14 +4,14 @@
 
 #for i in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216
 #do
-#	$1 --dense=$i --device=$2 $3 $4 $5
+#	$1 --dense=$i $2 $3 $4 $5
 #done
 
 #for i in `ls ../../graphs/*.mtx ../../graphs/spmv/*.mtx`
 for i in `ls /cygdrive/w/Dev/UFget/mtx/*.mtx`
 do 
-	$1 --mtx=`cygpath -w $i` --device=$2 $3 $4 $5
+	$1 --mtx=`cygpath -w $i` $2 $3 $4 $5
 done 
 
-#$1 --grid3d=300 --device=$2 $3 $4 $5
-#$1 --grid2d=4000 --device=$2 $3 $4 $5
+#$1 --grid3d=300 $2 $3 $4 $5
+#$1 --grid2d=4000 $2 $3 $4 $5
