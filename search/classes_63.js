257e5c753d5a08fecccb4b78cbfa908e40c7c79a
dumerrill <dumerrill@moochbot>
2015-12-15 18:38:19 -0500

    1.5.0 doc
    Former-commit-id: 53113a3c1829824af3ae05fd3cd774b03563c253

--- search/classes_63.js
+++ search/classes_63.js
@@ -5,6 +5,5 @@ var searchData=
   ['cachingdeviceallocator',['CachingDeviceAllocator',['../structcub_1_1_caching_device_allocator.html',1,'cub']]],
   ['cast',['Cast',['../structcub_1_1_cast.html',1,'cub']]],
   ['constantinputiterator',['ConstantInputIterator',['../classcub_1_1_constant_input_iterator.html',1,'cub']]],
-  ['countinginputiterator',['CountingInputIterator',['../classcub_1_1_counting_input_iterator.html',1,'cub']]],
-  ['cubvector',['CubVector',['../structcub_1_1_cub_vector.html',1,'cub']]]
+  ['countinginputiterator',['CountingInputIterator',['../classcub_1_1_counting_input_iterator.html',1,'cub']]]
 ];
--- search/classes_69.js
+++ search/classes_69.js
@@ -3,7 +3,6 @@ var searchData=
   ['if',['If',['../structcub_1_1_if.html',1,'cub']]],
   ['inequality',['Inequality',['../structcub_1_1_inequality.html',1,'cub']]],
   ['inequalitywrapper',['InequalityWrapper',['../structcub_1_1_inequality_wrapper.html',1,'cub']]],
-  ['int2type',['Int2Type',['../structcub_1_1_int2_type.html',1,'cub']]],
   ['ispointer',['IsPointer',['../structcub_1_1_is_pointer.html',1,'cub']]],
   ['isvolatile',['IsVolatile',['../structcub_1_1_is_volatile.html',1,'cub']]]
 ];
