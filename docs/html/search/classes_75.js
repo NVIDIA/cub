541967077a9e7ba0bf75e833ec436264452ba3dc
dumerrill <dumerrill@nvidia.com>
2015-12-28 12:59:22 -0500

    1.5.1 doc changes
    Former-commit-id: 821bba38b25da7bccbe62a9fef4f0d37542df749

--- docs/html/search/classes_75.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['uninitialized',['Uninitialized',['../structcub_1_1_uninitialized.html',1,'cub']]],
-  ['uninitialized_3c_20_5ftempstorage_20_3e',['Uninitialized&lt; _TempStorage &gt;',['../structcub_1_1_uninitialized.html',1,'cub']]]
-];
