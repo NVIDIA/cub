c508b4f67be7bb5fdfdb951037a0a348b2aed1d1
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2018-01-23 20:03:11 -0800

    Fix computation of valid lanes in warp-level reduction primitive to correctly handle the case where there are 0 input items per warp.

--- cub/warp/specializations/warp_reduce_shfl.cuh
+++ cub/warp/specializations/warp_reduce_shfl.cuh
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2011, Duane Merrill.  All rights reserved.
  * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the NVIDIA CORPORATION nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -347,13 +347,13 @@ struct WarpReduceShfl
         KeyValuePair<KeyT, ValueT> output;
 
         KeyT other_key = ShuffleDown<LOGICAL_WARP_THREADS>(input.key, offset, last_lane, member_mask);
-        
+
         output.key = input.key;
         output.value = ReduceStep(
-            input.value, 
-            cub::Sum(), 
-            last_lane, 
-            offset, 
+            input.value,
+            cub::Sum(),
+            last_lane,
+            offset,
             Int2Type<IsInteger<ValueT>::IS_SMALL_UNSIGNED>());
 
         if (input.key != other_key)
