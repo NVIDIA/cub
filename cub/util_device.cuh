10adf6c7f8d9b8a230ca3551e61f6b1eaf217062
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2019-10-10 14:44:19 -0700

    Cache the result of `cub::PtxVersion` to avoid expensive calls to `cudaFuncGetAttributes`, which acquires CUDA driver locks and performs poorly under contention.

--- cub/util_device.cuh
+++ cub/util_device.cuh
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2011, Duane Merrill.  All rights reserved.
  * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *     * Neither the name of the NVIDIA CORPORATION nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -141,6 +141,47 @@ CUB_RUNTIME_FUNCTION __forceinline__ cudaError_t PtxVersion(int &ptx_version)
 
     ptx_version = CUB_PTX_ARCH;
     return cudaSuccess;
+
+#elif __cplusplus >= 201103L
+
+    struct Cache
+    {
+        struct Result {
+          int const ptx_version;
+          cudaError_t const error;
+
+          Result(int ptx_version_, cudaError_t error_)
+              : ptx_version(ptx_version_), error(error_)
+          {}
+        };
+
+        Result result;
+
+        Cache()
+            : result(
+                  [] {
+                      int ptx_version = 0;
+                      cudaError_t error = cudaSuccess;
+                      do
+                      {
+                          cudaFuncAttributes empty_kernel_attrs;
+                          if (CubDebug(error = cudaFuncGetAttributes(&empty_kernel_attrs, EmptyKernel<void>))) break;
+                          ptx_version = empty_kernel_attrs.ptxVersion * 10;
+                      }
+                      while (0);
+
+                      return Result(ptx_version, error);
+                  }()
+              )
+        {}
+    };
+
+    static Cache cache;
+
+    if (!CubDebug(cache.result.error))
+        ptx_version = cache.result.ptx_version;
+
+    return cache.result.error;
 
 #else
 
