2fba463282b47140374caf0b77dc71fc4b5e6f2e
Allison Vacanti <alliepiper16@gmail.com>
2020-08-06 21:02:32 -0400

    Update the scan implementation to follow P0571's guidance.
    https://wg21.link/P0571

--- cub/device/device_scan.cuh
+++ cub/device/device_scan.cuh
@@ -158,10 +158,9 @@ struct DeviceScan
         // Signed integer type for global offsets
         typedef int OffsetT;
 
-        // The output value type
-        typedef typename If<(Equals<typename std::iterator_traits<OutputIteratorT>::value_type, void>::VALUE),  // OutputT =  (if output iterator's value type is void) ?
-            typename std::iterator_traits<InputIteratorT>::value_type,                                          // ... then the input iterator's value type,
-            typename std::iterator_traits<OutputIteratorT>::value_type>::Type OutputT;                          // ... else the output iterator's value type
+        // The output value type -- used as the intermediate accumulator
+        // Use the input value type per https://wg21.link/P0571
+        typedef typename std::iterator_traits<InputIteratorT>::value_type OutputT;
 
         // Initial value
         OutputT init_value = 0;
