899c82f7fa2c01b8563ce8dd926ca48f6f538608
Allison Vacanti <alliepiper16@gmail.com>
2021-05-20 16:46:21 -0400

    Update DeviceRadixSort docs to document bfloat16 support.

--- cub/device/device_radix_sort.cuh
+++ cub/device/device_radix_sort.cuh
@@ -62,8 +62,8 @@ namespace cub {
  *
  * \par Supported Types
  * DeviceRadixSort can sort all of the built-in C++ numeric primitive types
- * (<tt>unsigned char</tt>, \p int, \p double, etc.) as well as CUDA's \p __half
- * half-precision floating-point type.
+ * (`unsigned char`, `int`, `double`, etc.) as well as CUDA's `__half`
+ * and `__nv_bfloat16` 16-bit floating-point types.
  *
  * \par Floating-Point Special Cases
  *
