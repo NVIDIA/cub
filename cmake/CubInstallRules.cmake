2355b7f2f76c464ea3c501e49d2f891a38bd99a9
Robert Maynard <rmaynard@nvidia.com>
2022-03-31 11:05:45 -0400

    simplify the implementation

--- cmake/CubInstallRules.cmake
+++ cmake/CubInstallRules.cmake
@@ -22,8 +22,9 @@ install(DIRECTORY "${CUB_SOURCE_DIR}/cub/cmake/"
 )
 # Need to configure a file to store the infix specified in
 # CMAKE_INSTALL_INCLUDEDIR since it can be defined by the user
+set(install_location "${CMAKE_INSTALL_LIBDIR}/cmake/cub")
 configure_file("${CUB_SOURCE_DIR}/cub/cmake/cub-header-search.cmake.in"
   "${CUB_BINARY_DIR}/cub/cmake/cub-header-search.cmake"
   @ONLY)
 install(FILES "${CUB_BINARY_DIR}/cub/cmake/cub-header-search.cmake"
-  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/cub")
+  DESTINATION "${install_location}")
