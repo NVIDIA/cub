ea6a8c2ce4e20cc93920cb05ae8e968eff37d28c
Robert Maynard <robertjmaynard@gmail.com>
2022-07-20 21:39:10 -0400

    Update cub/cmake/cub-header-search.cmake
    Co-authored-by: Allison Vacanti <alliepiper16@gmail.com>

--- cub/cmake/cub-header-search.cmake
+++ cub/cmake/cub-header-search.cmake
@@ -1,5 +1,4 @@
 # Parse version information from version.h in source tree
- # Source tree
 set(_CUB_VERSION_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
 if(EXISTS "${_CUB_VERSION_INCLUDE_DIR}/cub/version.cuh")
   set(_CUB_VERSION_INCLUDE_DIR "${_CUB_VERSION_INCLUDE_DIR}" CACHE FILEPATH "" FORCE) # Clear old result
