84c02ebbe58ba5109770406cb62babd654df6087
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2022-10-09 04:21:48 +0400

    Optimize large segments

--- cub/device/dispatch/dispatch_reduce.cuh
+++ cub/device/dispatch/dispatch_reduce.cuh
@@ -380,6 +380,8 @@ __global__ void DeviceSegmentedReduceWithPartitioningKernel(
     const unsigned int *d_large_segments_indices,
     const unsigned int *d_medium_segments_indices,
     const unsigned int *d_small_segments_indices,
+    unsigned int *completed_blocks,
+    AccumT *completed_block_states,
     ReductionOpT reduction_op,
     InitT init)
 {
@@ -394,14 +396,6 @@ __global__ void DeviceSegmentedReduceWithPartitioningKernel(
                 ReductionOpT,
                 AccumT>;
 
-  using AgentMediumReduceT =
-    AgentWarpReduce<typename ActivePolicyT::MediumReducePolicy,
-                    InputIteratorT,
-                    OutputIteratorT,
-                    OffsetT,
-                    ReductionOpT,
-                    AccumT>;
-
   using AgentSmallReduceT =
     AgentWarpReduce<typename ActivePolicyT::SmallReducePolicy,
                     InputIteratorT,
@@ -410,23 +404,16 @@ __global__ void DeviceSegmentedReduceWithPartitioningKernel(
                     ReductionOpT,
                     AccumT>;
 
-  constexpr auto segments_per_medium_block =
-    static_cast<unsigned int>(ActivePolicyT::MediumReducePolicy::SEGMENTS_PER_BLOCK);
-  constexpr auto medium_threads_per_warp =
-    static_cast<unsigned int>(ActivePolicyT::MediumReducePolicy::WARP_THREADS);
-
   constexpr auto segments_per_small_block =
     static_cast<unsigned int>(ActivePolicyT::SmallReducePolicy::SEGMENTS_PER_BLOCK);
   constexpr auto small_threads_per_warp =
     static_cast<unsigned int>(ActivePolicyT::SmallReducePolicy::WARP_THREADS);
 
-  using MediumWarpReduce = cub::WarpReduce<AccumT, medium_threads_per_warp>;
   using SmallWarpReduce = cub::WarpReduce<AccumT, small_threads_per_warp>;
 
   // Shared memory storage
   __shared__ union {
-    typename AgentReduceT::TempStorage large_storage; 
-    typename AgentMediumReduceT::TempStorage medium_storage[segments_per_medium_block];
+    typename AgentReduceT::TempStorage large_and_medium_storage; 
     typename AgentSmallReduceT::TempStorage small_storage[segments_per_small_block];
   } temp_storage;
 
@@ -435,32 +422,51 @@ __global__ void DeviceSegmentedReduceWithPartitioningKernel(
 
   if (bid < large_blocks_end) 
   {
-    const unsigned int global_segment_id = d_large_segments_indices[bid];
+    const unsigned int n_sub_segments = ActivePolicyT::blocks_per_large_tile;
+    const unsigned int subsegment_id = bid % n_sub_segments;
+    const unsigned int large_segment_id = bid / n_sub_segments;
+    const unsigned int global_segment_id = d_large_segments_indices[large_segment_id];
 
     OffsetT segment_begin = d_begin_offsets[global_segment_id];
     OffsetT segment_end   = d_end_offsets[global_segment_id];
+    OffsetT segment_size = segment_end - segment_begin;
+
+    const OffsetT subsegment_size = segment_size / n_sub_segments;
+
+    OffsetT subsegment_begin = segment_begin + subsegment_size * subsegment_id;
+    OffsetT subsegment_end = subsegment_id < (n_sub_segments - 1) ? subsegment_begin + subsegment_size : segment_end;
 
     // Consume input tiles
-    AccumT block_aggregate = AgentReduceT(temp_storage.large_storage, d_in, reduction_op)
-                               .ConsumeRange(segment_begin, segment_end);
-
-    // Normalize as needed
-    NormalizeReductionOutput(block_aggregate, segment_begin, d_in);
+    AccumT block_aggregate = AgentReduceT(temp_storage.large_and_medium_storage, d_in, reduction_op)
+                                 .ConsumeRange(subsegment_begin, subsegment_end);
 
     if (tid == 0)
     {
-      d_out[global_segment_id] = reduction_op(init, block_aggregate);
+      completed_block_states += large_segment_id * n_sub_segments;
+      completed_block_states[subsegment_id] = block_aggregate;
+      __threadfence();
+      const unsigned int old_completed = atomicAdd(completed_blocks + large_segment_id, 1);
+
+      if (old_completed == n_sub_segments - 1) {
+        AccumT thread_aggregate = completed_block_states[0];
+
+        for (unsigned int sid = 1; sid < n_sub_segments; sid++) {
+          thread_aggregate = reduction_op(thread_aggregate, completed_block_states[sid]);
+        }
+
+        // Normalize as needed
+        NormalizeReductionOutput(thread_aggregate, segment_begin, d_in);
+
+        d_out[global_segment_id] = reduction_op(init, thread_aggregate);
+      }
     }
   } 
   else if (bid < medium_blocks_end) 
   {
-    const unsigned int sid_within_block = tid / medium_threads_per_warp;
-    const unsigned int medium_segment_id = (bid - large_blocks_end) * segments_per_medium_block +
-                                           sid_within_block;
+    const unsigned int medium_segment_id = bid - large_blocks_end;
 
     if (medium_segment_id < medium_segments)
     {
-      const unsigned int lane_id = tid % medium_threads_per_warp;
       const unsigned int global_segment_id = d_medium_segments_indices[medium_segment_id];
 
       OffsetT segment_begin = d_begin_offsets[global_segment_id];
@@ -468,13 +474,13 @@ __global__ void DeviceSegmentedReduceWithPartitioningKernel(
 
       // Consume input tiles
       AccumT warp_aggregate =
-        AgentMediumReduceT(temp_storage.medium_storage[sid_within_block], d_in, reduction_op, lane_id)
+        AgentReduceT(temp_storage.large_and_medium_storage, d_in, reduction_op)
           .ConsumeRange(segment_begin, segment_end);
 
       // Normalize as needed
       NormalizeReductionOutput(warp_aggregate, segment_begin, d_in);
 
-      if (lane_id == 0)
+      if (tid == 0)
       {
         d_out[global_segment_id] = reduction_op(init, warp_aggregate);
       }
@@ -546,6 +552,8 @@ struct DeviceReducePolicy
   //---------------------------------------------------------------------------
 
   /// SM35
+  // TODO Return
+#if 0
   struct Policy350 : ChainedPolicy<350, Policy350, Policy350>
   {
     static constexpr int threads_per_block  = 256;
@@ -568,25 +576,19 @@ struct DeviceReducePolicy
     // SegmentedReducePolicy
     using SegmentedReducePolicy = ReducePolicy;
 
-    // TODO Tune
-    using MediumReducePolicy = AgentWarpReducePolicy<ReducePolicy::BLOCK_THREADS,
-                                                     32 /* threads per warp */,
-                                                     16 /* items_per_thread */,
-                                                     AccumT,
-                                                     items_per_vec_load,
-                                                     LOAD_LDG>;
 
     // TODO Tune
     using SmallReducePolicy = AgentWarpReducePolicy<ReducePolicy::BLOCK_THREADS,
-                                                    1  /* threads per warp */,
+                                                    4  /* threads per warp */,
                                                     16 /* items_per_thread */,
                                                     AccumT,
                                                     items_per_vec_load,
                                                     LOAD_LDG>;
   };
+#endif
 
   /// SM60
-  struct Policy600 : ChainedPolicy<600, Policy600, Policy350>
+  struct Policy600 : ChainedPolicy<600, Policy600, Policy600 /* Policy350 */>
   {
     static constexpr int threads_per_block  = 256;
     static constexpr int items_per_thread   = 16;
@@ -608,16 +610,14 @@ struct DeviceReducePolicy
     using SegmentedReducePolicy = ReducePolicy;
 
     // TODO Tune
-    using MediumReducePolicy = AgentWarpReducePolicy<ReducePolicy::BLOCK_THREADS,
-                                                     16 /* threads per warp */,
-                                                     16 /* items_per_thread */,
-                                                     AccumT,
-                                                     items_per_vec_load,
-                                                     LOAD_LDG>;
+    static constexpr int blocks_per_large_tile = 2;
+
+    // TODO Tune
+    static constexpr int medium_tile_size = 2 * ReducePolicy::BLOCK_THREADS * items_per_thread;
 
     // TODO Tune
     using SmallReducePolicy = AgentWarpReducePolicy<ReducePolicy::BLOCK_THREADS,
-                                                    1  /* threads per warp */,
+                                                    4  /* threads per warp */,
                                                     16 /* items_per_thread */,
                                                     AccumT,
                                                     items_per_vec_load,
@@ -1363,7 +1363,7 @@ struct DispatchSegmentedReduce : SelectedPolicy
 
     do
     {
-      cub::detail::temporary_storage::layout<4> temporary_storage_layout;
+      cub::detail::temporary_storage::layout<6> temporary_storage_layout;
 
 #ifndef CUB_RDC_ENABLED
       constexpr static int num_selected_groups = 2;
@@ -1382,9 +1382,11 @@ struct DispatchSegmentedReduce : SelectedPolicy
       auto large_and_medium_partitioning_slot = temporary_storage_layout.get_slot(1);
       auto small_partitioning_slot = temporary_storage_layout.get_slot(2);
       auto group_sizes_slot = temporary_storage_layout.get_slot(3);
+      auto completed_blocks_slot = temporary_storage_layout.get_slot(4);
+      auto completed_block_states_slot = temporary_storage_layout.get_slot(5);
 
       LargeSegmentsSelectorT large_segments_selector(
-        ActivePolicyT::MediumReducePolicy::ITEMS_PER_TILE,
+        ActivePolicyT::medium_tile_size,
         d_begin_offsets,
         d_end_offsets);
 
@@ -1400,6 +1402,8 @@ struct DispatchSegmentedReduce : SelectedPolicy
       auto small_segments_indices =
         small_partitioning_slot->create_alias<unsigned int>();
       auto group_sizes = group_sizes_slot->create_alias<unsigned int>();
+      auto completed_blocks = completed_blocks_slot->create_alias<unsigned int>();
+      auto completed_block_states = completed_block_states_slot->create_alias<AccumT>();
 
       std::size_t three_way_partition_temp_storage_bytes {};
 
@@ -1407,6 +1411,9 @@ struct DispatchSegmentedReduce : SelectedPolicy
         large_and_medium_segments_indices.grow(num_segments);
         small_segments_indices.grow(num_segments);
         group_sizes.grow(num_selected_groups);
+
+        completed_blocks.grow(num_segments);
+        completed_block_states.grow(num_segments * ActivePolicyT::blocks_per_large_tile);
 
         auto medium_indices_iterator =
           THRUST_NS_QUALIFIER::make_reverse_iterator(
@@ -1493,10 +1500,12 @@ struct DispatchSegmentedReduce : SelectedPolicy
           static_cast<unsigned int>(num_segments) -
           (large_segments + small_segments);
 
-        const unsigned int large_blocks = large_segments;
+        const unsigned int large_blocks = large_segments * ActivePolicyT::blocks_per_large_tile;
         const unsigned int small_blocks = DivideAndRoundUp(small_segments, ActivePolicyT::SmallReducePolicy::SEGMENTS_PER_BLOCK);
-        const unsigned int medium_blocks = DivideAndRoundUp(medium_segments, ActivePolicyT::MediumReducePolicy::SEGMENTS_PER_BLOCK);
+        const unsigned int medium_blocks = medium_segments;
         const unsigned int total_blocks = large_blocks + medium_blocks + small_blocks;
+
+        cudaMemsetAsync(completed_blocks.get(), 0, sizeof(unsigned int) * num_segments, stream);
 
         #ifdef CUB_DETAIL_DEBUG_ENABLE_LOG
         _CubLog("Invoking DeviceSegmentedReduceWithPartitioningKernel<<<%d, %d, 0, %lld>>>(), "
@@ -1527,6 +1536,8 @@ struct DispatchSegmentedReduce : SelectedPolicy
                 large_and_medium_segments_indices.get(),
                 large_and_medium_segments_indices.get() + num_segments - medium_segments,
                 small_segments_indices.get(),
+                completed_blocks.get(),
+                completed_block_states.get(),
                 reduction_op,
                 init);
 
