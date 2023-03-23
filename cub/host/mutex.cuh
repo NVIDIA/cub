71677949e8d5721504dbef4089a3a83a32090e49
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2023-03-23 13:38:45 +0400

    Deprecate cub::Mutex

--- cub/host/mutex.cuh
+++ cub/host/mutex.cuh
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Copyright (c) 2011, Duane Merrill.  All rights reserved.
- * Copyright (c) 2011-2018, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2023, NVIDIA CORPORATION.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -31,45 +31,24 @@
  * Simple portable mutex
  */
 
-#include "../util_cpp_dialect.cuh"
-
 #pragma once
 
-#if CUB_CPP_DIALECT >= 2011
-    #include <mutex>
-#else
-    #if defined(_WIN32) || defined(_WIN64)
-        #include <intrin.h>
+#include <mutex>
 
-        #define WIN32_LEAN_AND_MEAN
-        #define NOMINMAX
-        #include <windows.h>
-        #undef WIN32_LEAN_AND_MEAN
-        #undef NOMINMAX
-
-        /**
-         * Compiler read/write barrier
-         */
-        #pragma intrinsic(_ReadWriteBarrier)
-
-    #endif
-#endif
-
-#include "../config.cuh"
+#include <cub/config.cuh>
+#include <cub/util_deprecated.cuh>
 
 
 CUB_NAMESPACE_BEGIN
 
 
 /**
- * Simple portable mutex
- *   - Wraps std::mutex when compiled with C++11 or newer (supported on all platforms)
- *   - Uses GNU/Windows spinlock mechanisms for pre C++11 (supported on x86/x64 when compiled with cl.exe or g++)
+ * Wraps std::mutex 
+ * @deprecated [Since CUB 2.1.0] The `cub::Mutex` is deprecated and will be removed 
+ *             in a future release. Use `std::mutex` instead.
  */
-struct Mutex
+struct CUB_DEPRECATED Mutex
 {
-#if CUB_CPP_DIALECT >= 2011
-
     std::mutex mtx;
 
     void Lock()
@@ -81,82 +60,7 @@ struct Mutex
     {
         mtx.unlock();
     }
-
-#else       // C++11
-
-    #if CUB_HOST_COMPILER == CUB_HOST_COMPILER_MSVC
-
-        // Microsoft VC++
-        typedef long Spinlock;
-
-    #else
-
-        // GNU g++
-        typedef int Spinlock;
-
-        /**
-         * Compiler read/write barrier
-         */
-        __forceinline__ void _ReadWriteBarrier()
-        {
-            __sync_synchronize();
-        }
-
-        /**
-         * Atomic exchange
-         */
-        __forceinline__ long _InterlockedExchange(volatile int * const Target, const int Value)
-        {
-            // NOTE: __sync_lock_test_and_set would be an acquire barrier, so we force a full barrier
-            _ReadWriteBarrier();
-            return __sync_lock_test_and_set(Target, Value);
-        }
-
-        /**
-         * Pause instruction to prevent excess processor bus usage
-         */
-        __forceinline__ void YieldProcessor()
-        {
-        }
-
-    #endif  // MSVC
-
-        /// Lock member
-        volatile Spinlock lock;
-
-        /**
-         * Constructor
-         */
-        Mutex() : lock(0) {}
-
-        /**
-         * Return when the specified spinlock has been acquired
-         */
-        __forceinline__ void Lock()
-        {
-            while (1)
-            {
-                if (!_InterlockedExchange(&lock, 1)) return;
-                while (lock) YieldProcessor();
-            }
-        }
-
-
-        /**
-         * Release the specified spinlock
-         */
-        __forceinline__ void Unlock()
-        {
-            _ReadWriteBarrier();
-            lock = 0;
-        }
-
-#endif      // C++11
-
 };
 
 
-
-
 CUB_NAMESPACE_END
-
