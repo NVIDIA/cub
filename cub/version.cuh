23113d33d04e6a3c12816f9718d7f1cb6d0a2613
Allison Vacanti <alliepiper16@gmail.com>
2023-04-14 11:59:54 -0400

    Bump version to 2.1.1.

--- cub/version.cuh
+++ cub/version.cuh
@@ -43,7 +43,7 @@
  *         <tt>CUB_VERSION / 100 % 1000</tt> is the minor version.
  *         <tt>CUB_VERSION / 100000</tt> is the major version.
  */
-#define CUB_VERSION 200100
+#define CUB_VERSION 200101
 
 /*! \def CUB_MAJOR_VERSION
  *  \brief The preprocessor macro \p CUB_MAJOR_VERSION encodes the
