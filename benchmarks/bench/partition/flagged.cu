eb9731a1b521235d9857b4ca14798ab0ea000b3b
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2023-05-13 11:12:31 +0400

    Fewer workloads for select and partition

--- benchmarks/bench/partition/flagged.cu
+++ benchmarks/bench/partition/flagged.cu
@@ -138,4 +138,4 @@ NVBENCH_BENCH_TYPES(partition, NVBENCH_TYPE_AXES(fundamental_types, offset_types
   .set_name("cub::DevicePartition::Flagged")
   .set_type_axes_names({"T{ct}", "OffsetT{ct}"})
   .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4))
-  .add_string_axis("Entropy", {"1.000", "0.811", "0.544", "0.337", "0.201", "0.000"});
+  .add_string_axis("Entropy", {"1.000", "0.544", "0.000"});
